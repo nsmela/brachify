34554f7f9108b49166ee7933db02271a42b05f4a
nsmela <nsmela@hotmail.com>
2023-11-01 14:44:07 -0700

    Update random_testing.py
    filling gaps algo is working

--- src/random_testing.py
+++ src/random_testing.py
@@ -2,6 +2,8 @@ from OCC.Core.gp import gp_Pnt, gp_Circ, gp_Dir, gp_Ax2, gp_Vec
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakePolygon, BRepBuilderAPI_MakeEdge, BRepBuilderAPI_MakeWire, BRepBuilderAPI_MakeFace, BRepBuilderAPI_MakeSolid
 from OCC.Core.GeomAbs import GeomAbs_Arc
 from OCC.Core.BRepOffsetAPI import BRepOffsetAPI_MakeEvolved, BRepOffsetAPI_ThruSections
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Fuse
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakePrism
 from OCC.Core.GC import GC_MakeArcOfCircle
 from OCC.Core.BRepFill import BRepFill_PipeShell
 
@@ -18,7 +20,6 @@ def evolved_shape():
     angle_rads = math.radians(angle) 
     height = 160
     length = 200
-    length_offset = 20.0
     radius_offset = 35.0
     radius = 5.0
     tip_thickness = 20.0
@@ -46,7 +47,7 @@ def evolved_shape():
     edge1 = BRepBuilderAPI_MakeEdge(p0, p1).Edge()
 
     # extra length on the straight tube going outside the cylinder
-    p2 = gp_Pnt(0,0,length + length_offset)
+    p2 = gp_Pnt(0,0,length)
     edge2 = BRepBuilderAPI_MakeEdge(p1,p2).Edge()
 
     # curved section to match tandem's tilt
@@ -104,7 +105,8 @@ def evolved_shape():
     pipe_straight.Add(circle_wire)
     pipe_straight.Build()
     pipe_straight.MakeSolid()
-    display.DisplayShape(pipe_straight.Shape())
+    #display.DisplayShape(pipe_straight.Shape())
+    display.DisplayShape(straight_wire)
 
     circle_edge = BRepBuilderAPI_MakeEdge(gp_Circ(gp_Ax2(p1, gp_Dir(0,0,1)), radius))
     circle_wire = BRepBuilderAPI_MakeWire(circle_edge.Edge()).Wire()
@@ -112,16 +114,19 @@ def evolved_shape():
     pipe_curved.Add(circle_wire)
     pipe_curved.Build()
     pipe_curved.MakeSolid()
-    display.DisplayShape(pipe_curved.Shape())
+    #display.DisplayShape(pipe_curved.Shape())
+    display.DisplayShape(curve_wire)
 
     circle_edge = BRepBuilderAPI_MakeEdge(gp_Circ(gp_Ax2(p3, gp_Dir(vec)), tip_radius))
     circle_wire = BRepBuilderAPI_MakeWire(circle_edge.Edge()).Wire()
-    pipe_tip = BRepFill_PipeShell(BRepBuilderAPI_MakeWire(edge4).Wire())
+    wire = BRepBuilderAPI_MakeWire(edge4).Wire()
+    pipe_tip = BRepFill_PipeShell(wire)
     pipe_tip.Add(circle_wire)
     pipe_tip.Build()
     pipe_tip.MakeSolid()
     pipe_tip_shape = BRepBuilderAPI_MakeSolid(pipe_tip.Shape()).Shape()
-    display.DisplayShape(pipe_tip_shape)
+    #display.DisplayShape(pipe_tip_shape)
+    display.DisplayShape(wire)
 
     wire = BRepBuilderAPI_MakeWire(edge5).Wire()
     circle_edge = BRepBuilderAPI_MakeEdge(gp_Circ(gp_Ax2(p5, gp_Dir(vec)), tip_radius))
@@ -131,7 +136,32 @@ def evolved_shape():
     pipe_tip2 = BRepOffsetAPI_ThruSections()
     pipe_tip2.AddWire(circle_wire)
     pipe_tip2.AddWire(circle_wire2)
-    display.DisplayShape(pipe_tip2.Shape())
+    #display.DisplayShape(pipe_tip2.Shape())
+    display.DisplayShape(wire)
+
+    # making polygons to fill spaces between cylinders
+    # lower half
+    # edge from edge1 to bottom of tandem tips
+    slope, b = get_perpindicular_line(p3, p4)
+    y = get_y_on_line(0, slope, b)
+    p7 = gp_Pnt(0, 0, y)
+    edge6 = BRepBuilderAPI_MakeEdge(p3, p7).Edge()
+    edge7 = BRepBuilderAPI_MakeEdge(p1, p7).Edge()
+    wire = BRepBuilderAPI_MakeWire(edge3, edge6, edge7).Wire()
+    face = BRepBuilderAPI_MakeFace(wire).Face()
+    prism = BRepPrimAPI_MakePrism(face, gp_Vec(0, radius, 0)).Shape()
+    prism = BRepAlgoAPI_Fuse(prism, BRepPrimAPI_MakePrism(face, gp_Vec(0, -radius, 0)).Shape()).Shape()
+    display.DisplayShape(prism)
+
+    # upper half
+    edge7 = BRepBuilderAPI_MakeEdge(p3, p5).Edge()
+    edge8 = BRepBuilderAPI_MakeEdge(p4, p6).Edge()
+    wire = BRepBuilderAPI_MakeWire(edge4, edge8, edge5, edge7).Wire()
+    face = BRepBuilderAPI_MakeFace(wire).Face()
+    prism = BRepPrimAPI_MakePrism(face, gp_Vec(0, tip_radius, 0)).Shape()
+    prism = BRepAlgoAPI_Fuse(prism, BRepPrimAPI_MakePrism(face, gp_Vec(0, -tip_radius, 0)).Shape()).Shape()
+    display.DisplayShape(prism)
+    
 
 if __name__ == "__main__":
     evolved_shape()
