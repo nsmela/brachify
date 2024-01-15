41b97379eaf197601a6c0eb8e73a790fe472e268
nsmela <nsmela@hotmail.com>
2024-01-15 09:46:12 -0800

    added a main function to tandem to facilitate testing

--- src/classes/mesh/tandem.py
+++ src/classes/mesh/tandem.py
@@ -219,4 +219,9 @@ class Line:
 
 
 if __name__ == "__main__":
-    generate_tandem()
+    from OCC.Display.SimpleGui import init_display
+
+    display, start_display, add_menu, add_function_to_menu = init_display()
+
+    display.DisplayShape(generate_tandem())
+    start_display()
